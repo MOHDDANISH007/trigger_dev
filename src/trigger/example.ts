87ff94818f25b8053e23232562634a855bcd4b87
Mohd Danish <m.danish00707@gmail.com>
2026-04-07 00:48:09 +0530

    added webhook site

--- src/trigger/example.ts
+++ src/trigger/example.ts
@@ -2,24 +2,40 @@ import { logger, task, wait } from "@trigger.dev/sdk/v3";
 
 export const helloWorldTask = task({
   id: "hello-world",
-  // Set an optional maxDuration to prevent tasks from running indefinitely
-  maxDuration: 300, // Stop executing after 300 secs (5 mins) of compute
+  maxDuration: 300,
+
   run: async (payload: any, { ctx }) => {
     logger.log("Hello, world!", { payload, ctx });
 
-    const senddata : any = {
+    const senddata: any = {
       payload,
-      "password": "supersecret",
-      "username": "superuser"
-    }
+      password: "supersecret",
+      username: "superuser",
+    };
 
     console.log(`payload: ${JSON.stringify(payload)}`);
 
+    // ⏳ wait for 5 seconds
     await wait.for({ seconds: 5 });
+
+    // 🔥 Send data to webhook
+    const response = await fetch(
+      "https://webhook.site/4b0910fa-fcea-414d-8e1b-c0a48ce268b8",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(senddata),
+      }
+    );
+
+    const result = await response.text();
 
     return {
       message: "Hello, world!",
-      senddata
-    }
+      webhookResponse: result,
+      senddata,
+    };
   },
 });
